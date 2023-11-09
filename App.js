a6354f14ec05f650152e1ede9e922d6c4761e4ce
chucamo <khoaivu2k3@gmail.com>
2023-11-09 23:40:28 +0700

    Nectar

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Number from './src/componnets/Number';
+import Onbording from './src/componnets/Onbording';
+import Sing_in from './src/componnets/Sing_in';
+import Splash_screen from './src/componnets/Splash_screen';
+import Verification from './src/componnets/Verification';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { useState } from 'react';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator>
+
+      <Stack.Screen name="Splash_screen" component={Splash_screen} options={{headerShown:false}}/>
+      <Stack.Screen name="Onbording" component={Onbording} options={{headerShown:false}} />
+      <Stack.Screen name="Sing_in" component={Sing_in} options={{headerShown:false}} />
+      <Stack.Screen name="Number" component={Number} options={{headerShown:false}} />
+      <Stack.Screen name="Verification" component={Verification} options={{headerShown:false}}/>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
