e512fe423e52c9c0eba2b2097e163d8fa2c7370d
chucamo <khoaivu2k3@gmail.com>
2023-11-16 11:22:02 +0700

    Bar-Code-Scan-App

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Number from './src/componnets/Number';
-import Onbording from './src/componnets/Onbording';
-import Sing_in from './src/componnets/Sing_in';
-import Splash_screen from './src/componnets/Splash_screen';
-import Verification from './src/componnets/Verification';
+import Cart from './src/screens/Cart';
+import Home from './src/screens/Home';
+import Scanf from './src/screens/Scanf';
+import NavigationTag from './src/screens/NavigationTag';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { useState } from 'react';
+
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -15,12 +15,10 @@ export default function App() {
   return (
     <NavigationContainer>
      <Stack.Navigator>
-
-      <Stack.Screen name="Splash_screen" component={Splash_screen} options={{headerShown:false}}/>
-      <Stack.Screen name="Onbording" component={Onbording} options={{headerShown:false}} />
-      <Stack.Screen name="Sing_in" component={Sing_in} options={{headerShown:false}} />
-      <Stack.Screen name="Number" component={Number} options={{headerShown:false}} />
-      <Stack.Screen name="Verification" component={Verification} options={{headerShown:false}}/>
+      <Stack.Screen name="NavigationTag" component={NavigationTag} options={{headerShown:false}}/>
+      {/* <Stack.Screen name="Home" component={Home} options={{headerShown:false}}/> */}
+      <Stack.Screen name="Scanf" component={Scanf} options={{headerShown:false}}/>
+      {/* <Stack.Screen name="Cart" component={Cart} options={{headerShown:false}}/> */}
     </Stack.Navigator>
     </NavigationContainer>
   );
